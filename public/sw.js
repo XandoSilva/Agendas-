0bfea9f9076b330e5af3ca75e2b7c20298b3d1c6
Alessandro Souza <adasilvasouza@gmail.com>
2026-08-16 17:27:55 -0300

    fix(pwa): force update service worker to clear iOS cache

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'vero-ops-v2';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => caches.delete(key))
+      );
+    }).then(() => {
+      self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
